7c792f43ab2c1586a6d4d5fde0d122c66bec84d1
Ruy Adorno <ruyadorno@hotmail.com>
2018-03-17 22:09:15 -0400

    Adding matcher tests wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const promisify = require('util').promisify;
+const { promisify } = require('util');
 const path = require('path');
 const exec = promisify(require('child_process').exec);
 const access = promisify(require('fs').access);
@@ -13,11 +13,11 @@ const MASTER_REGEX_MORE_EXTENSIONS = /(\/?([a-z.A-Z0-9\-_]+\/)+[+@a-zA-Z0-9\-_+.
 const HOMEDIR_REGEX = /(~\/([a-z.A-Z0-9\-_]+\/)+[@a-zA-Z0-9\-_+.]+\.[a-zA-Z0-9]{1,10})[:-]{0,1}(\d+)?/;
 const OTHER_BGS_RESULT_REGEX = /(\/?([a-z.A-Z0-9\-_]+\/)+[a-zA-Z0-9_.]{3,})[:-]{0,1}(\d+)/;
 const ENTIRE_TRIMMED_LINE_IF_NOT_WHITESPACE = /(\S.*\S|\S)/;
-const JUST_FILE = /([@+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10})(\s|$|:)+/;
-const JUST_FILE_WITH_SPACES = /([a-zA-Z][@+a-z. A-Z0-9\-_]+\.[a-zA-Z]{1,10})(\s|$|:)+/;
-const FILE_NO_PERIODS = /(((\/?([a-z.A-Z0-9\-_]+\/))?\.[a-zA-Z0-9\-_]{3,}[a-zA-Z0-9\-_\/]*) | ([a-z.A-Z0-9\-_\/]{1,}\/[a-zA-Z0-9\-_]{1,}) | ([A-Z][a-zA-Z]{2,}file)) (\s|$|:)+/;
-const MASTER_REGEX_WITH_SPACES_AND_WEIRD_FILES = /((?:\.?\/)?(([a-z.A-Z0-9\-_]|\s[a-zA-Z0-9\-_])+\/)+((\/?([a-z.A-Z0-9\-_]+\/))?\.[a-zA-Z0-9\-_]{3,}[a-zA-Z0-9\-_\/]*) | ([a-z.A-Z0-9\-_\/]{1,}\/[a-zA-Z0-9\-_]{1,}) | ([A-Z][a-zA-Z]{2,}file))/;
-const MASTER_REGEX_WITH_SPACES = /((?:\.?\/)?(([a-z.A-Z0-9\-_]|\s[a-zA-Z0-9\-_])+\/)+([\(\),@a-zA-Z0-9\-_+.]|\s[,\(\)@a-zA-Z0-9\-_+.])+\.[a-zA-Z0-9-]{1,30}) [:-]{0,1}(\d+)?/;
+const JUST_FILE = /([@%+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10})(\s|$|:)+/;
+const JUST_FILE_WITH_SPACES = /([a-zA-Z][%@+a-z. A-Z0-9\-_]+\.[a-zA-Z]{1,10})(\s|$|:)+/;
+const FILE_NO_PERIODS = /(((\/?([a-z.A-Z0-9\-_]+\/))?\.[a-zA-Z0-9\-_]{3,}[a-zA-Z0-9\-_\/]*)|([a-z.A-Z0-9\-_\/]{1,}\/[a-zA-Z0-9\-_]{1,})|([A-Z][a-zA-Z]{2,}file))(\s|$|:)+/;
+const MASTER_REGEX_WITH_SPACES_AND_WEIRD_FILES = /((?:\.?\/)?(([a-z.A-Z0-9\-_]|\s[a-zA-Z0-9\-_])+\/)+((\/?([a-z.A-Z0-9\-_]+\/))?\.[a-zA-Z0-9\-_]{3,}[a-zA-Z0-9\-_\/]*)|([a-z.A-Z0-9\-_\/]{1,}\/[a-zA-Z0-9\-_]{1,})|([A-Z][a-zA-Z]{2,}file))/;
+const MASTER_REGEX_WITH_SPACES = /((?:\.?\/)?(([a-z.A-Z0-9\-_]|\s[a-zA-Z0-9\-_])+\/)+([\(\),@a-zA-Z0-9\-_+.]|\s[,\(\)@a-zA-Z0-9\-_+.])+\.[a-zA-Z0-9-]{1,30})[:-]{0,1}(\d+)?/;
 
 const REGEX_WATERFALL = [
 	{
@@ -59,92 +59,67 @@ const REGEX_WATERFALL = [
 ];
 
 const unpackMatches = matches =>
-	matches && matches.length ? matches[0] : null;
+	matches && matches.length > 1 ? matches[1] : null;
 
 const getRootPath = () =>
 	exec(GIT_ROOT_CMD)
 		.catch(() => exec(HG_ROOT_CMD))
-		.catch(() => './');
+		.catch(() => ({ stdout: './' }))
+		.then(({ stdout }) => stdout);
 
-const prependDir = (file, withFileInspection = true) => {
+const prependDir = file => {
 	const splitUp = file.split(path.sep);
 	const [firstPart] = splitUp;
 	const firstChars = file.substr(0, 2);
-	let prependPath;
-	let topLevelPath;
-	let relativePath;
-	let hasTopLevel;
-	let hasRelative;
 
-	return getRootPath()
-		.then(rootPath => {
-			prependPath = path.join(rootPath.trim(), '/');
-			topLevelPath = path.join(prependPath, splitUp);
-			relativePath = path.join('.', splitUp);
-		})
-		.then(access(topLevelPath))
-		.then(() => {
-			hasTopLevel = true;
-		})
-		.catch(() => {
-			hasTopLevel = false;
-		})
-		.then(access(relativePath))
-		.then(() => {
-			hasRelative = true;
-		})
-		.catch(() => {
-			hasRelative = false;
-		})
-		.then(() => {
-			if (
-				!file ||
-				file.length < 2 ||
-				file[0] === '/' ||
-				file.substr(0, 2) === './' ||
-				file.substr(0, 3) === '../' ||
-				file.substr(0, 4) === '.../'
-			)
-				return file;
-			if (file.substr(0, 2) === '~/') return untildify(file);
-			if (firstPart === 'home') return path.join('/', file);
-			if (firstPart === 'www') return untildify(path.join('~/', file));
-			if (file.indexOf('/') === -1) return path.join('./', file);
-			if (firstChars === 'a/' || firstChars === 'b/')
-				return path.join(prependPath, file.substr(2));
-			if (!withFileInspection) return path.join(prependPath, file);
-			if (!hasTopLevel && hasRelative) return relativePath;
-			return topLevelPath;
-		});
+	return getRootPath().then(rootPath => {
+		const prependPath = path.join(rootPath.trim(), '/');
+		if (
+			file.length < 2 ||
+			file[0] === '/' ||
+			firstChars === './' ||
+			file.substr(0, 3) === '../' ||
+			file.substr(0, 4) === '.../'
+		)
+			return file;
+		if (firstChars === '~/') return untildify(file);
+		if (firstPart === 'home') return path.join('/', file);
+		if (firstPart === 'www') return untildify(path.join('~/', file));
+		if (file.indexOf('/') === -1) return path.join('./', file);
+		if (firstChars === 'a/' || firstChars === 'b/')
+			return path.join(prependPath, file.substr(2));
+		return path.join(prependPath, file);
+	});
 };
 
 const matcher = ({ line, validateFileExists, allInput }) =>
-	REGEX_WATERFALL.reduce(
-		(acc, { allInput, regex, validateFileExists, preferred }) => {
-			if (
-				(allInput && !allInput) ||
-				(allInput && !allInput) ||
-				(validateFileExists && !validateFileExists) ||
-				!regex.test(line)
-			)
-				return;
+	REGEX_WATERFALL.reduce((acc, regexConfig) => {
+		if (
+			(regexConfig.allInput && !allInput) ||
+			(!regexConfig.allInput && allInput) ||
+			(regexConfig.validateFileExists && !validateFileExists) ||
+			!regexConfig.regex.test(line)
+		)
+			return acc;
 
-			const matches = regex.exec(line);
+		const matches = regexConfig.regex.exec(line);
 
-			if (!preferred) return acc.concat(unpackMatches(matches));
-			if (!preferred.test(line)) return acc.concat(unpackMatches(matches));
-
-			const preferredMatches = preferred.exec(line);
-
-			if (preferredMatches.index < matches.index)
-				return acc.concat(unpackMatches(preferredMatches));
-
+		if (!regexConfig.preferred || !regexConfig.preferred.test(line))
 			return acc.concat(unpackMatches(matches));
-		},
-		[]
-	).filter(a => a);
 
-function pickAPath({ line, validateFileExists = false, allInput = false }) {
+		const preferredMatches = regexConfig.preferred.exec(line);
+
+		if (preferredMatches.index < matches.index)
+			return acc.concat(unpackMatches(preferredMatches));
+
+		return acc.concat(unpackMatches(matches));
+	}, []).filter(a => a);
+
+function pickAPath(line, { validateFileExists = false, allInput = false }) {
+	if (typeof line !== 'string') {
+		throw new TypeError('ERR_INVALID_ARG_TYPE');
+	}
+
 	if (!validateFileExists) {
 		return Promise.resolve(matcher({ line, allInput })[0] || null);
 	}
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const exec = require('child_process').exec;
+const cp = require('child_process');
 
 const {
 	unpackMatches,
@@ -21,17 +21,24 @@ jest.mock('child_process', () => {
 		setHgFail(value) {
 			hgFail = value;
 		},
-		exec: cmd => {
-			if (cmd === GIT_ROOT_CMD && !gitFail) {
-				return Promise.resolve(expectedRootFolder);
-			} else if (cmd === HG_ROOT_CMD && !hgFail) {
-				return Promise.resolve(expectedRootFolder);
-			} else {
-				return Promise.reject(new Error('ENOENT'));
-			}
+		exec: (cmd, cb) => {
+			require('util')
+				.promisify(setImmediate)()
+				.then(() => {
+					if (cmd === GIT_ROOT_CMD && !gitFail) {
+						cb(null, { stdout: expectedRootFolder });
+					} else if (cmd === HG_ROOT_CMD && !hgFail) {
+						cb(null, { stdout: expectedRootFolder });
+					} else {
+						cb(new Error('ENOENT'));
+					}
+				})
+				.catch(() => cb(new Error('ENOENT')));
 		}
 	};
 });
+
+jest.mock('untildify', () => i => i.replace(/^~/, '/Users/username'));
 
 describe('unpackMatches', () => {
 	it('should return null if non-valid value used', () => {
@@ -46,26 +53,516 @@ describe('unpackMatches', () => {
 		expect(unpackMatches({})).toBe(null);
 	});
 	it('should return unpacked match if valid value used', () => {
-		expect(unpackMatches(['foo'])).toBe('foo');
-		expect(unpackMatches(/[a-z]/.exec('foo'))).toBe('f');
-		expect(unpackMatches(/([a-z]).([a-z])/.exec('--foo-bar'))).toBe('foo');
+		expect(unpackMatches(['', 'foo'])).toBe('foo');
+		expect(unpackMatches(/([a-z])/.exec('foo'))).toBe('f');
+		expect(unpackMatches(/([a-z].[a-z])/.exec('--foo-bar'))).toBe('foo');
 	});
 });
 
 describe('getRootPath', () => {
+	afterAll(() => {
+		cp.setGitFail(false);
+		cp.setHgFail(false);
+	});
 	it('should return expected root folder on getRootPath from git', () => {
-		exec.setGitFail = false;
-		exec.setHgFail = false;
-		expect(getRootPath()).resolves.toBe('/Users/username/Documents/foo');
+		cp.setGitFail(false);
+		cp.setHgFail(false);
+		expect.assertions(1);
+		return expect(getRootPath()).resolves.toBe('/Users/username/Documents/foo');
 	});
 	it('should return expected root folder on getRootPath from hg', () => {
-		exec.setGitFail = true;
-		exec.setHgFail = false;
-		expect(getRootPath()).resolves.toBe('/Users/username/Documents/foo');
+		cp.setGitFail(true);
+		cp.setHgFail(false);
+		expect.assertions(1);
+		return expect(getRootPath()).resolves.toBe('/Users/username/Documents/foo');
 	});
 	it('should return expected root folder on getRootPath from default', () => {
-		exec.setGitFail = true;
-		exec.setHgFail = false;
-		expect(getRootPath()).resolves.toBe('.');
+		cp.setGitFail(true);
+		cp.setHgFail(true);
+		expect.assertions(1);
+		return expect(getRootPath()).resolves.toBe('./');
+	});
+});
+
+describe('prependDir', () => {
+	it('should return same input value for less than 2 chars values', () => {
+		return expect(prependDir('.')).resolves.toBe('.');
+	});
+	it('should return same input value for absolute paths', () => {
+		return expect(prependDir('/foo/bar')).resolves.toBe('/foo/bar');
+	});
+	it('should return same input value for dot started paths', () => {
+		return expect(prependDir('./foo/bar')).resolves.toBe('./foo/bar');
+	});
+	it('should return same input value for double dot started paths', () => {
+		return expect(prependDir('../foo/bar')).resolves.toBe('../foo/bar');
+	});
+	it('should return same input value for triple dot started paths', () => {
+		return expect(prependDir('.../foo')).resolves.toBe('.../foo');
+	});
+	it('should return expanded home folder paths', () => {
+		return expect(prependDir('~/Document/workspace')).resolves.toBe(
+			'/Users/username/Document/workspace'
+		);
+	});
+	it('should return corrected value for home folders', () => {
+		return expect(prependDir('home/username/foo')).resolves.toBe(
+			'/home/username/foo'
+		);
+	});
+	it('should return corrected value for www folders', () => {
+		return expect(prependDir('www/foo')).resolves.toBe(
+			'/Users/username/www/foo'
+		);
+	});
+	it('should return dot prepend value for relative single values', () => {
+		return expect(prependDir('foo')).resolves.toBe('foo');
+	});
+	it('should return corrected values for git-diff-like values a/', () => {
+		return expect(prependDir('a/foo/bar')).resolves.toBe(
+			'/Users/username/Documents/foo/foo/bar'
+		);
+	});
+	it('should return corrected values for git-diff-like values b/', () => {
+		return expect(prependDir('b/foo/bar')).resolves.toBe(
+			'/Users/username/Documents/foo/foo/bar'
+		);
+	});
+	it('should simply return top level value if file inspection fails', () => {
+		return expect(prependDir('foo/bar/item')).resolves.toBe(
+			'/Users/username/Documents/foo/foo/bar/item'
+		);
+	});
+});
+
+describe('matcher', () => {
+	it('should return expected output for every given input', () => {
+		[
+			{
+				input: { line: 'html/js/hotness.js' },
+				output: ['html/js/hotness.js', 'hotness.js']
+			},
+			{
+				input: { line: '/absolute/path/to/something.txt' },
+				output: ['/absolute/path/to/something.txt', 'something.txt', '.txt']
+			},
+			{
+				input: { line: '/html/js/hotness.js42' },
+				output: ['/html/js/hotness.js42', '/html/js/hotness.js4', '.js42']
+			},
+			{
+				input: { line: '/html/js/hotness.js' },
+				output: ['/html/js/hotness.js', 'hotness.js']
+			},
+			{
+				input: { line: './asd.txt:83' },
+				output: ['./asd.txt', './asd.txt', 'asd.txt', '.txt']
+			},
+			{
+				input: { line: '.env.local' },
+				output: ['.env.local', '.local']
+			},
+			{
+				input: { line: '.gitignore' },
+				output: ['.gitignore']
+			},
+			{
+				input: { line: 'tmp/.gitignore' },
+				output: ['tmp/.gitignore']
+			},
+			{
+				input: { line: '.ssh/.gitignore' },
+				output: ['.ssh/.gitignore']
+			},
+			{
+				input: { line: '.ssh/known_hosts' },
+				output: ['.ssh/known_hosts']
+			},
+			{
+				input: { line: '.a' },
+				output: []
+			},
+			{
+				input: { line: 'flib/asd/ent/berkeley/two.py-22' },
+				output: [
+					'flib/asd/ent/berkeley/two.py',
+					'flib/asd/ent/berkeley/two.py',
+					'.py-22'
+				]
+			},
+			{
+				input: { line: 'flib/foo/bar' },
+				output: ['flib/foo/bar']
+			},
+			{
+				input: { line: 'flib/foo/bar ' },
+				output: ['flib/foo/bar']
+			},
+			{
+				input: { line: 'foo/b ' },
+				output: ['foo/b']
+			},
+			{
+				input: { line: 'foo/bar/baz/' },
+				output: []
+			},
+			{
+				input: { line: 'flib/ads/ads.thrift' },
+				output: ['flib/ads/ads.thrift', 'ads.thrift', '.thrift']
+			},
+			{
+				input: { line: 'banana hanana Wilde/ads/story.m' },
+				output: ['Wilde/ads/story.m', 'story.m']
+			},
+			{
+				input: { line: 'flib/asd/asd.py two/three/four.py' },
+				output: ['flib/asd/asd.py', 'asd.py']
+			},
+			{
+				input: { line: 'asd/asd/asd/ 23' },
+				output: []
+			},
+			{
+				input: { line: 'foo/bar/TARGETS:23' },
+				output: ['foo/bar/TARGETS', 'foo/bar/TARGETS']
+			},
+			{
+				input: { line: 'foo/bar/TARGETS-24' },
+				output: ['foo/bar/TARGETS', 'foo/bar/TARGETS-24']
+			},
+			{
+				input: {
+					line:
+						'fbcode/search/places/scorer/PageScorer.cpp:27:46:#include "search/places/scorer/linear_scores/MinutiaeVerbScorer.h'
+				},
+				output: [
+					'fbcode/search/places/scorer/PageScorer.cpp',
+					'fbcode/search/places/scorer/PageScorer.cpp',
+					'PageScorer.cpp',
+					'.cpp'
+				]
+			},
+			{
+				input: {
+					line:
+						'fbcode/search/places/scorer/TARGETS:590:28:    srcs = ["linear_scores/MinutiaeVerbScorer.cpp"]'
+				},
+				output: [
+					'fbcode/search/places/scorer/TARGETS',
+					'fbcode/search/places/scorer/TARGETS',
+					'fbcode/search/places/scorer/TARGETS'
+				]
+			},
+			{
+				input: {
+					line:
+						'fbcode/search/places/scorer/TARGETS:1083:27:      "linear_scores/test/MinutiaeVerbScorerTest.cpp"'
+				},
+				output: [
+					'fbcode/search/places/scorer/TARGETS',
+					'fbcode/search/places/scorer/TARGETS',
+					'fbcode/search/places/scorer/TARGETS'
+				]
+			},
+			{
+				input: { line: '~/foo/bar/something.py' },
+				output: [
+					'~/foo/bar/something.py',
+					'/foo/bar/something.py',
+					'something.py'
+				]
+			},
+			{
+				input: { line: '~/foo/bar/inHomeDir.py:22' },
+				output: [
+					'~/foo/bar/inHomeDir.py',
+					'/foo/bar/inHomeDir.py',
+					'/foo/bar/inHomeDir.py',
+					'inHomeDir.py'
+				]
+			},
+			{
+				input: { line: 'blarge assets/retina/victory@2x.png' },
+				output: ['assets/retina/victory@2x.png', 'victory@2x.png', '.png']
+			},
+			{
+				input: { line: '~/assets/retina/victory@2x.png' },
+				output: [
+					'~/assets/retina/victory@2x.png',
+					'/assets/retina/victory@2x.png',
+					'victory@2x.png',
+					'.png'
+				]
+			},
+			{
+				input: { line: 'So.many.periods.txt' },
+				output: ['So.many.periods.txt', '.txt']
+			},
+			{
+				input: { line: 'SO.MANY.PERIODS.TXT' },
+				output: ['SO.MANY.PERIODS.TXT', '.TXT']
+			},
+			{
+				input: { line: 'blarg blah So.MANY.PERIODS.TXT:22' },
+				output: ['So.MANY.PERIODS.TXT', '.TXT']
+			},
+			{
+				input: { line: 'SO.MANY&&PERIODSTXT' },
+				output: []
+			},
+			{
+				input: { line: 'test src/categories/NSDate+Category.h' },
+				output: ['src/categories/NSDate+Category.h', 'NSDate+Category.h']
+			},
+			{
+				input: { line: '~/src/categories/NSDate+Category.h' },
+				output: [
+					'~/src/categories/NSDate+Category.h',
+					'/src/categories/NSDate+Category.h',
+					'NSDate+Category.h'
+				]
+			},
+			{
+				input: {
+					line: 'M    ./inputs/evilFile With Space.txt',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/evilFile With Space.txt',
+					'./inputs/evilFile',
+					'Space.txt',
+					'evilFile With Space.txt',
+					'./inputs/evilFile'
+				]
+			},
+			{
+				input: {
+					line: './inputs/evilFile With Space.txt:22',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/evilFile With Space.txt',
+					'./inputs/evilFile',
+					'Space.txt',
+					'evilFile With Space.txt',
+					'./inputs/evilFile'
+				]
+			},
+			{
+				input: {
+					line: './inputs/annoying Spaces Folder/evilFile With Space2.txt',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying Spaces Folder/evilFile With Space2.txt',
+					'./inputs/annoying',
+					'Space2.txt',
+					'evilFile With Space2.txt',
+					'./inputs/annoying'
+				]
+			},
+			{
+				input: {
+					line: './inputs/annoying Spaces Folder/evilFile With Space2.txt:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying Spaces Folder/evilFile With Space2.txt',
+					'./inputs/annoying',
+					'Space2.txt',
+					'evilFile With Space2.txt',
+					'./inputs/annoying'
+				]
+			},
+			{
+				input: {
+					line: ' ./inputs/annoying Spaces Folder/evilFile With Space2.txt:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying Spaces Folder/evilFile With Space2.txt',
+					'./inputs/annoying',
+					'Space2.txt',
+					'evilFile With Space2.txt',
+					'./inputs/annoying'
+				]
+			},
+			{
+				input: {
+					line:
+						'M     ./inputs/annoying Spaces Folder/evilFile With Space2.txt:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying Spaces Folder/evilFile With Space2.txt',
+					'./inputs/annoying',
+					'Space2.txt',
+					'evilFile With Space2.txt',
+					'./inputs/annoying'
+				]
+			},
+			{
+				input: { line: 'M     ./objectivec/NSArray+Utils.h' },
+				output: ['./objectivec/NSArray+Utils.h', 'NSArray+Utils.h']
+			},
+			{
+				input: { line: 'NSArray+Utils.h' },
+				output: ['NSArray+Utils.h']
+			},
+			{
+				input: {
+					line: './inputs/NSArray+Utils.h:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/NSArray+Utils.h',
+					'./inputs/NSArray+Utils.h',
+					'./inputs/NSArray+Utils.h',
+					'./inputs/NSArray',
+					'NSArray+Utils.h',
+					'NSArray+Utils.h'
+				]
+			},
+			{
+				input: {
+					line: './inputs/blogredesign.sublime-workspace:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/blogredesign.sublime',
+					'./inputs/blogredesign.sublime-workspace',
+					'./inputs/blogredesign.sublime-workspace',
+					'./inputs/blogredesign',
+					'.sublime-workspace'
+				]
+			},
+			{
+				input: {
+					line: 'inputs/blogredesign.sublime-workspace:42',
+					validateFileExists: true
+				},
+				output: [
+					'inputs/blogredesign.sublime',
+					'inputs/blogredesign.sublime-workspace',
+					'inputs/blogredesign.sublime-workspace',
+					'inputs/blogredesign',
+					'.sublime-workspace'
+				]
+			},
+			{
+				input: {
+					line: './inputs/annoying-hyphen-dir/Package Control.system-bundle',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying-hyphen-dir/Package Control.system-bundle',
+					'./inputs/annoying-hyphen-dir/Package',
+					'./inputs/annoying-hyphen-dir/Package'
+				]
+			},
+			{
+				input: {
+					line: 'inputs/annoying-hyphen-dir/Package Control.system-bundle',
+					validateFileExists: true
+				},
+				output: [
+					'inputs/annoying-hyphen-dir/Package Control.system-bundle',
+					'inputs/annoying-hyphen-dir/Package',
+					'inputs/annoying-hyphen-dir/Package'
+				]
+			},
+			{
+				input: {
+					line: './inputs/annoying-hyphen-dir/Package Control.system-bundle:42',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/annoying-hyphen-dir/Package Control.system-bundle',
+					'./inputs/annoying-hyphen-dir/Package',
+					'./inputs/annoying-hyphen-dir/Package'
+				]
+			},
+			{
+				input: {
+					line: './inputs/svo (install the zip, not me).xml',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/svo (install the zip, not me).xml',
+					'./inputs/svo',
+					'./inputs/svo'
+				]
+			},
+			{
+				input: {
+					line: './inputs/svo (install the zip not me).xml',
+					validateFileExists: true
+				},
+				output: [
+					'./inputs/svo (install the zip not me).xml',
+					'./inputs/svo',
+					'./inputs/svo'
+				]
+			},
+			{
+				input: {
+					line: './inputs/svo install the zip, not me.xml',
+					validateFileExists: true
+				},
+				output: ['./inputs/svo install the zip, not me.xml']
+			},
+			{
+				input: {
+					line: './inputs/svo install the zip not me.xml',
+					validateFileExists: true
+				},
+				output: ['./inputs/svo install the zip not me.xml']
+			},
+			{
+				input: {
+					line: './inputs/annoyingTildeExtension.txt~:42',
+					validateFileExists: true
+				},
+				output: ['./inputs/annoyingTildeExtension.txt~']
+			},
+			{
+				input: { line: 'inputs/.DS_KINDA_STORE', validateFileExists: true },
+				output: ['inputs/.DS_KINDA_STORE']
+			},
+			{
+				input: { line: './inputs/.DS_KINDA_STORE', validateFileExists: true },
+				output: ['./inputs/.DS_KINDA_STORE']
+			},
+			{
+				input: { line: 'evilFile No Prepend.txt', validateFileExists: true },
+				output: ['evilFile No Prepend.txt']
+			},
+			{
+				input: { line: 'file-from-yocto_%.bbappend', validateFileExists: true },
+				output: ['file-from-yocto_%.bbappend']
+			},
+			{
+				input: {
+					line: 'otehr thing ./foo/file-from-yocto_3.1%.bbappend',
+					validateFileExists: true
+				},
+				output: ['file-from-yocto_3.1%.bbappend']
+			},
+			{
+				input: {
+					line: './file-from-yocto_3.1%.bbappend',
+					validateFileExists: true
+				},
+				output: ['./file-from-yocto_3.1%.bbappend']
+			},
+			{
+				input: { line: 'Gemfile' },
+				output: ['Gemfile']
+			},
+			{
+				input: { line: 'Gemfilenope' },
+				output: []
+			}
+		].forEach(({ input, output }) => expect(matcher(input)).toEqual(output));
 	});
 });
